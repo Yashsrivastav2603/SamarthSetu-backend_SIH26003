cc8c8e992472213de4d03734b5ab9099e83f7338
Yashsrivastav2603 <yashsrivastav2603@gmail.com>
2026-09-08 13:48:17 +0530

    Add MongoDB env var diagnostic + fix property name

--- src/main/java/com/SamarthSetu/backend/BackendApplication.java
+++ src/main/java/com/SamarthSetu/backend/BackendApplication.java
@@ -7,14 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class BackendApplication {
 
     public static void main(String[] args) {
-        String mongoUri = System.getenv("MONGODB_URI");
-
-        System.out.println("MONGODB_URI exists: " + (mongoUri != null));
-        if (mongoUri != null) {
-            System.out.println("MONGODB_URI length: " + mongoUri.length());
-            System.out.println("MONGODB_URI starts with mongodb+srv://: " + mongoUri.startsWith("mongodb+srv://"));
-        }
-
         SpringApplication.run(BackendApplication.class, args);
     }
 }
